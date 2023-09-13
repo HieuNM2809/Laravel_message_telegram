7ff7a58417e74ae2bae141aa8e067dd88611fbbe
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-09-13 09:24:56 +0700

    thêm cách gọi qua curl

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,8 +9,26 @@ class TelegramController extends Controller
 {
     public function updatedActivity()
     {
-        $activity = Telegram::getUpdates();
-        dd($activity);
+//        $proxyUrl = 'http://proxy.hcm.fpt.vn'; // Địa chỉ proxy và cổng
+//        $activity = Telegram::getUpdates([
+//            CURLOPT_PROXY => $proxyUrl,
+//            CURLOPT_PROXYPORT => 80,
+//        ]);
+
+        $ch = curl_init('https://api.telegram.org/bot6288276952:AAGIn1-nuvuyZ-L4gzt0bcx5_19cAO_o0vQ/getUpdates');
+        $proxyUrl = 'http://proxy.hcm.fpt.vn:80';
+        curl_setopt($ch, CURLOPT_PROXY, $proxyUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $activity = curl_exec($ch);
+
+        if ($activity === false) {
+            echo 'Lỗi cURL: ' . curl_error($ch);
+        }
+        curl_close($ch);
+
+
+        dd(json_decode($activity, true));
+
     }
     public function contactForm()
     {
@@ -29,11 +47,39 @@ class TelegramController extends Controller
             . "<b>Message: </b>\n"
             . " - " . $request->message;
 
-        Telegram::sendMessage([
-            'chat_id' => env('TELEGRAM_CHANNEL_ID', '-1001738435883'),
-            'parse_mode' => 'HTML',
-            'text' => $text
-        ]);
+
+        $curl = curl_init();
+        $proxyUrl = 'http://proxy.hcm.fpt.vn:80';
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.telegram.org/bot6288276952:AAGIn1-nuvuyZ-L4gzt0bcx5_19cAO_o0vQ/sendMessage',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 10, // Timeout 10 giây
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+                "chat_id": "-1001738435883",
+                "parse_mode": "HTML",
+                "text": "'.$text.'"
+            }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            CURLOPT_PROXY => $proxyUrl,
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        dd(json_decode($response, true));
+
+
+
+//        Telegram::sendMessage([
+//            'chat_id' => env('TELEGRAM_CHANNEL_ID', '-1001738435883'),
+//            'parse_mode' => 'HTML',
+//            'text' => $text
+//        ]);
 
         return redirect()->back();
     }
